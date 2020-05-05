4a08e4eb9c6eda290f0329a396ca0603b051ae03
LloydTao <LL23@live.co.uk>
2020-05-05 17:17:00 +0100

    Create main function to use script

--- src/main.py
+++ src/main.py
@@ -41,3 +41,41 @@ def export_canvas(logo, filename):
         print("Saved file to disk!")
     except IOError:
         print("Unable to create file on disk.")
+
+
+if __name__ == "__main__":
+
+    # Get inputs.
+    string = input('Logo text: ')
+    stops = []
+    colours = int(input('Number of gradient colours: '))
+    for i in range(colours):
+        offset = "{:.0%}".format(i / (colours - 1))
+        colour = input('Gradient colour: ')
+        stops.append({"offset": offset,"color": colour})
+    graphic = input('Graphic file: ')
+    name = input('Logo name: ')
+
+    # Define styles.
+    text_styles = ''.join(
+        [
+            'font-size:128px;',
+            'font-family: Rammetto One, Arial;',
+            'font-weight:800;',
+            'text-transform: uppercase;',
+            
+            'stroke:black;',
+            'stroke-width:8;'
+            
+            'background: linear-gradient(to right, #30CFD0 0%, #330867 100%);',
+            '-webkit-background-clip: text;',
+            '-webkit-text-fill-color: transparent;',
+        ]
+    )
+    
+    # Create logo.
+    logo = create_canvas()
+    logo.defs.add(gradient(stops))
+    logo = add_text(logo, string, text_styles)
+    logo = add_graphic(logo, "graphics/dogs/" + graphic)
+    export_canvas(logo, name + ".svg")
